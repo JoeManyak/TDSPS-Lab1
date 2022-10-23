dd1003655acb496efff37a92f499fb1370252b85
joe <doncrafttv@gmail.com>
2022-10-23 16:48:31 +0300

    Get by user and categories records

--- models/record.go
+++ models/record.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"lab1/models/category"
+	"lab1/models/user"
+	"time"
+)
 
 var idCount = 0
 var records []Record
@@ -13,6 +17,44 @@ type Record struct {
 	Sum        float64
 }
 
-func Create(UserID int, CategoryID int, now time.Time, sum float64) {
-	//todo
+func Create(userID int, categoryID int, now time.Time, sum float64) error {
+	_, err := user.GetByID(userID)
+	if err != nil {
+		return err
+	}
+
+	_, err = category.GetByID(categoryID)
+	if err != nil {
+		return err
+	}
+
+	records = append(records, Record{
+		ID:         idCount,
+		UserID:     userID,
+		CategoryID: categoryID,
+		Timestamp:  now,
+		Sum:        sum,
+	})
+	idCount++
+	return nil
+}
+
+func GetByUser(id int) []Record {
+	var result []Record
+	for i := range records {
+		if records[i].UserID == id {
+			result = append(result, records[i])
+		}
+	}
+	return result
+}
+
+func GetByUserAndCategory(userID, categoryID int) []Record {
+	var result []Record
+	for i := range records {
+		if records[i].UserID == userID && records[i].CategoryID == categoryID {
+			result = append(result, records[i])
+		}
+	}
+	return result
 }
